2509bb744fd20ef35db7de8740ad7463564f4c41
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-04-07 15:47:57 +0530

    added changes

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 ## Installation without Docker
 
-    conda create --name env_name python=3.8
+    conda create --name <env_name> python=3.8
     pip install Flask
     conda install -c rdkit rdkit
 
 To run the application, use 
 
-    flask --app app run --debug
-You can skip the `--debug` flag if you do not want to run the app in debug mode
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py runserver 
 
 ## Installation with Docker
 If you have docker installed on your system, you can run the app directly using docker
 Use the following command to do so
 
-    docker-compose up -d
+    docker-compose up -d --build 
 You can leave out `-d` flag if you do not wish to run in detached mode
+
+> On successfully running the server, go to http://127.0.0.1:5000/depict/ to check out the portal.
--- depict/views.py
+++ depict/views.py
@@ -9,23 +9,32 @@ from rest_framework import status
 from .utils import *
 
 def index(request):
-    return render(request, "depict/test.html")
+    return render(request, "depict/index.html")
 
 @api_view(['GET', 'POST'])
 def get_mols(request, type):
     input_text, datas = get_content(type, request)
-
     output = []
+    row_output = []
+    counter = 0
+    print(input_text)
     for d in datas:      
         smile, name = d.split(" ")
         comp = Chem.MolFromSmiles(smile)
         AllChem.Compute2DCoords(comp)
         svg = show(comp)
-        output.append([svg, name])
+        counter += 1
+        print(svg, counter)
+        row_output.append([svg, name])
+        if counter == 3:
+            output.append(row_output)
+            row_output = []
+            counter = 0
+    output.append(row_output)
     
     context = {
         "images": output,
         "input_text": input_text
     }
 
-    return render(request, "depict/test.html", context = context)
+    return render(request, "depict/index.html", context = context)
