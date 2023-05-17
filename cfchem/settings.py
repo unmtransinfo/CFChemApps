db621edf26baea537f3bbd6ab79fdd2bfff850a3
Priyansh Kedia <kediapriyansh@gmail.com>
2023-05-17 01:02:27 -0600

    minor change

--- cfchem/settings.py
+++ cfchem/settings.py
@@ -148,3 +148,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
