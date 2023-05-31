25c72cf8d50e6f6d28ea01ae1ee1a19789711ddc
Priyansh Kedia <kediapriyansh@gmail.com>
2023-05-31 17:39:37 -0400

    minor change

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+# python manage.py migrate
 
 # Start server
 echo "Starting server"
