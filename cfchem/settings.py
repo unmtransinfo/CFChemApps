9477e07a30113509dae2106e3a55856c454edb66
Priyansh-Kedia <kediapriyansh@gmail.com>
2024-01-05 09:08:34 -0500

    minor changes in staticfiles settings

--- cfchem/settings.py
+++ cfchem/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
