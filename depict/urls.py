be5ebc3f1f6a7aa1874e923178c73f6fad05abb0
Jack-42 <johnjack0987@gmail.com>
2024-02-15 12:08:38 -0700

    fix urls for 'request_type' instead of 'type'

--- depict/urls.py
+++ depict/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("mols/<str:type>/", views.get_mols, name = "get_mols"),
+    path("mols/<str:request_type>/", views.get_mols, name = "get_mols"),
     path("help/", views.help, name = "help")
 ]
