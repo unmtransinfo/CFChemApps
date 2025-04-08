0e282b6c29923fc1189670d01bac21f300e7d87b
Bivek Sharma Panthi <sharmabivek12@gmail.com>
2025-04-08 13:03:07 -0600

    Update entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Applying database migrations"
-python manage.py migrate --noinput
+# echo "Applying database migrations"
+# python manage.py migrate --noinput
 
 echo "Collecting static files"
 python manage.py collectstatic --noinput
