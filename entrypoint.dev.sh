c80a25e43b459affecf9343b668d389c62c4bede
bivek2003 <sharmabivek12@gmail.com>
2025-04-09 15:32:37 -0600

    changes are ok.

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -7,6 +7,9 @@ python manage.py makemigrations
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Collecting static files"
+python manage.py collectstatic --noinput
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
